69411b54e64ac016077184868fa430a32f7d0880
cta-source <source@cta.tech>
2021-10-26 08:48:30 -0400

    Update syncsupport.py
    Modified correlation peak search and audio data unpacking ; changed some operations from .copy to .view for speed; removed signal conditional routines (not helpful for short segment times).

--- syncsupport/src/syncsupport.py
+++ syncsupport/src/syncsupport.py
@@ -19,61 +19,13 @@ import argparse
 import sys
 from _portaudio import paInt16
 
-# Signal conditioning window types.  
-NONE_TYPE = 0
-BARTLETT_TYPE = 1
-BLACKMAN_TYPE = 2
-HAMMING_TYPE = 3
-HANNING_TYPE = 4
-RAISED_COSINE_TYPE = 4
-KAISER_TYPE = 5 # Currently not used, if we want it need to add the beta param
+#import matplotlib.pyplot as plt   # For when we want to plot intermediate curves
 
 def nextpowerof2(value):
     if value < 2:
         return 0
     next2 = 2**(math.ceil(math.log2(value-1))) 
     return next2
-
-def applywindow(audiosegment, cyclicpad, windowtype):
-    # Accepts
-    # 1) a segment of audio; 
-    # 2) an integer cyclicpad, units of (1/samplerate) samples (default � audio segment length; must be no longer than the audio segment length else error)
-    # 3) A window type field (0 = no window; 1 = Hann/raised cosine; other values currently unused).
-    # Pads the original segment: for array So[0..N-1], creates a new segment array Sp[ ] 
-    # such that Sp[0..cyclicpad-1] = So[N-cp..N-1]; Sp[N..N+cyclipad-1] = So[0..cyclicpad-1]
-    # Applies the specified window function with taper length cyclicpad
-
-    # Check to see if we are not using windowing.  
-    # Windowing doesn't help for very short segments (e.g. 20mS), so it is not used for most tests.
-    if windowtype == NONE_TYPE:
-        return audiosegment
-
-    # Pad the passed audiosegment 
-    windowdata = audiosegment[(len(audiosegment)-cyclicpad):]
-    windowdata = np.append(windowdata, audiosegment)
-    windowdata = np.append(windowdata, audiosegment[0:cyclicpad-1])  
-    windowdata = audiosegment
-
-    # Retrieve the selected window function values
-    if windowtype == HANNING_TYPE:
-        windowvalues = np.hanning(len(audiosegment))
-    elif windowtype == BARTLETT_TYPE:
-        windowvalues = np.bartlett(len(audiosegment))
-    elif windowtype == BLACKMAN_TYPE:
-        windowvalues = np.blackman(len(audiosegment))
-    elif windowtype == HAMMING_TYPE:
-        windowvalues = np.hamming(len(audiosegment))
-    elif windowtype == KAISER_TYPE:
-        print("Warning: Kaiser filter is not implemented; falling back to Hanning.")
-        windowvalues = np.hanning(len(audiosegment))
-
-    # Apply the selected window to the padded data
-    # todo: windowdata is int16, windowvalues are floats.  Upconvert windowdata, multiply, scale back to int16.
-    #np.multiply(windowdata[:cyclicpad-1], windowvalues[:cyclicpad-1])
-    #np.multiply(windowdata[cyclicpad + len(audiosegment):], windowvalues[cyclicpad+1:]) # Note the '+1' is to skip the middle value
-    windowdata = np.multiply(audiosegment, windowvalues) 
-        
-    return windowdata
 
 def getsegmentfromtime(subjectdata, checktime, observationperiod):
     # subjectdata: an audio file with embedded PN code, from which a slice of OP length will be taken,
@@ -86,27 +38,19 @@ def getsegmentfromtime(subjectdata, checktime, observationperiod):
     segmentdata = subjectdata[checktime:checktime + observationperiod - 1].copy()
     return segmentdata
 
-def gettimefromsegment(subjectdata, observationsegment, cyclicpad, windowtype):
+def gettimefromsegment(subjectdata, segmentdata):
     # Accepts 
     # 1) subjectdata: audio data in which we will search for a PN-based timestamp, 
     # 2) observationsegment: a slice of audio embedding a PN timestamp (i.e., should be from an audio file with encoded PN sequencing).  Length of observationsegment is the observation period OP. 
-    # 3) cyclicpad: A cyclic prefix length for windowing
-    # 4) windowtype: Window function type for applywindow, with:
-    #   BARTLETT_TYPE = 0; BLACKMAN_TYPE = 1; HAMMING_TYPE = 2; HANNING_TYPE = 3; 
-    #   RAISED_COSINE_TYPE = 3; KAISER_TYPE = 4 # Currently not used, if we want it need to add the beta param
     # Returns
     # 1) A resulting time value tR (scalar int) in (1/samplerate) units.  The interpretation of this time value
     # is,     The value tD is referenced to t0, the beginning of the subject data, and is the delay from t0 until the first  
     #         sample of the segment appears when matched up with the subject data; the value tD is in (1/samplerate) units, 
     #         where a positive tD indicates the segment appears later than t0, and negative tD implies the segment 
     #         is cut off by trying to start before t0. 
-    # This method applies applywindow() to observationsegment to prepare (pad and window) the observationsegment for the FFT.  Finds
-    # the presentation time in subjectfile in which the windowed observationsegment appears; and returns the presentation 
-    # time of the beginning of the original observationsegment (not counting the cyclic prefix) in subjectfile.
-
-    # Apply the window function.  If cyclicpad == 0, don't apply cyclic padding.  If windowtype == NONE_TYPE, don't apply windowing.
-    segmentdata = applywindow(observationsegment, cyclicpad, windowtype)
+    # This method finds the media time where the observationsegment appears in the target segmentdata.
     
+    # (Timing information: the next block (to RESULTDATA) takes about 0.03s on length_result 262144.)
     # Cross-correlate the two data sets to find where the segmentdata appears in the subjectdata.
     length_result  =  nextpowerof2(len(subjectdata))
     SUBJECTDATA = np.fft.fft(subjectdata, n=length_result, norm="ortho")
@@ -114,9 +58,15 @@ def gettimefromsegment(subjectdata, observationsegment, cyclicpad, windowtype):
     RESULTDATA = np.multiply(SUBJECTDATA, np.conj(SEGMENTDATA))
 
     resultcomplex = (np.fft.ifft(RESULTDATA, n=length_result, norm="ortho"))
-    resultdata = resultcomplex.real
-    result_tuple = np.where(resultdata == np.amax(resultdata))
-    result = result_tuple[0]
+    resultdata = np.absolute(resultcomplex)
+    result_tuple = np.where(resultdata == np.amax(np.absolute(resultdata)))
+    result = result_tuple[0][0]
+
+    # Plot for debug purposes
+    #result_xaxis = np.arange(0, len(resultdata), 1)
+    #plt.title("Correlation")
+    #plt.plot(result_xaxis, resultdata)
+    #plt.show()    
 
     return result
 
@@ -192,72 +142,69 @@ def opendatafile(filename, verifyhash):
     channels     = wf.getnchannels()
     samplerate   = wf.getframerate()
 
-    # Read the data into an array 
-    myframes    = wf.getnframes()  # scalar
-    myframesbig = wf.readframes(myframes)   # byte string
+    # Read the data into an array.
+    framecount = wf.getnframes()  # scalar
+    framestring = wf.readframes(framecount)   # byte string
+    unpackstring = '{0}h{0}h'.format(framecount) 
+    framelist = list(struct.unpack(unpackstring, framestring)) 
+    framesaschannels = np.array(framelist, dtype=np.short, ndmin=2)
+    framesonlyLch = np.reshape(framesaschannels, (channels,framecount), 'F')
 
-    unpackstring = '<{0}h'.format(myframes*channels) 
-    channeldata = list(struct.unpack(unpackstring, myframesbig)) # Convert the byte string into a list of ints
- 
     p.terminate()   # End of pyaudio session
 
     # Return data includes channeldata but only the part that represents the L channel.
-    return (samplerate, channeldata[0::channels], channels, sampleformat, hashresult)
+    return (samplerate, framesonlyLch, channels, sampleformat, hashresult)
 
-def trimaudio(subjectdata, segmentdata, cyclicpad, windowtype):
+def trimaudio(subjectdata, segmentdata):
     # Accepts 
     # 1) subjectdata: a first audio file that was presumably recorded with some dead time before the audio of interest, 
     # 2) segmentdata:  a segment of PN audio that should mark the end of the audio of interest, 
-    # 3) cyclicpad: A cyclic prefix length for windowing
-    # 4) windowtype: Window function type for applywindow
     # Returns: a shorter copy of subjectdata with leading audio trimmed.  The portio trimmed is the part
     # prior to first occurance of segmentdata.
 
     # Align the archived copy of PN data (segmentdata) with the PN data in the recorded audio (sujectdata) 
-    result = gettimefromsegment(subjectdata, segmentdata, cyclicpad, windowtype)
+    result = gettimefromsegment(subjectdata, segmentdata[0:960])
     # Trim the front and back of the recorded audio to get rid of useless extra recording time
-    trimmeddata = subjectdata[result[0]:(len(segmentdata)+result[0])].copy()
+    trimmeddata = subjectdata[result:(len(segmentdata)+result)].copy()
  
     return trimmeddata
-
-def findsegmentorder(subjectfile, segmentfile, observationperiod, cyclicpad, neighborhood):
+   
+def findsegmentorder(subjectfile, segmentfile, observationperiod, neighborhood):
     # Accepts 
     # 1) subjectfile: a first (e.g.PN or recorded) audio file, possibly with extra audio before the "real" data, 
     # 2) segmentfile: a second (e.g. recorded or PN) audio file, 
     # 3) observationperiod: a duration interpreted as a multiple of (1/samplerate), 
-    # 4) cyclicpad: A cyclic prefix length for windowing
-    # 5) neighborhood: A range, in units of (1/samplerate) seconds, in which to look for a match
+    # 4) neighborhood: A range, in units of (1/samplerate) seconds, in which to look for a match
     # Steps through segmentfile in observationperiod increments, and repeatedly calls gettimefromsegment to find the presentation time of each increment.
     # Returns an array of presentation times.  
-
+    
     # Open the PN sequence file (archived pseudo noise sequence in audio format), check hash, and extract the L channel
     segmentinfo = opendatafile(segmentfile, verifyhash=True)
-    segmentdataPN    = segmentinfo[1]
+    segmentdataPN    = segmentinfo[1][0]
     segmentrate      = segmentinfo[0]   # Should be 48000 for the WAVE project 
     segmentformat    = segmentinfo[3]   # Should be 16bit PCM
     hashresult       = segmentinfo[4]
-    segmentduration   = math.floor(len(segmentdataPN)//(segmentrate))
+    segmentduration   = math.floor((len(segmentdataPN))//(segmentrate))
     if segmentformat != paInt16:
         print("Error, PN file %s format is not Int16", subjectfile) 
         sys.exit(-1)   
+    
     if hashresult != True:
         print("Error, PN file %s appears corrupted (failed hash check)", subjectfile) 
         sys.exit(-1)   
 
     # Open the subject file (recorded audio played out by the device under test) and extract the L channel
     subjectinfo    = opendatafile(subjectfile, verifyhash=False)
-    rawfiledata    = subjectinfo[1]
+    rawfiledata    = subjectinfo[1][0]
     samplerate     = subjectinfo[0]
     sampleformat   = subjectinfo[3] # Can also fetch channels as subjectinfo[2] if needed
     
     # Trim off any leading (useless) audio prior to the watermarked portion
-    #segmentdatafront = segmentdataPN[0:(2*samplerate)].copy()   # Use the first two seconds of PN data
-    #segmentdatarear  = segmentdataPN[len(segmentdataPN)-(2*samplerate):].copy()   # Use the last two seconds of PN data
-    filedata = trimaudio(rawfiledata, segmentdataPN, cyclicpad, NONE_TYPE)
+    filedata = trimaudio(rawfiledata, segmentdataPN)
     
-    duration   = math.floor(len(filedata)/samplerate)
+    duration   = math.floor((len(filedata))/samplerate)
     if duration != segmentduration:
-        print("Error, the two files have different durations, should be 60 seconds") 
+        print("Error, the two files have different durations, should be 60 seconds--possibly less than 60s of audio from start of watermark to end of file, or inadequate audio SNR") 
         sys.exit(-1)   
     if samplerate != segmentrate:
         print("Error, the two files have different sample rates, should be 48000") 
@@ -265,21 +212,27 @@ def findsegmentorder(subjectfile, segmentfile, observationperiod, cyclicpad, nei
     if sampleformat != segmentformat:
         print("Error, the two files have different formats; should be 16b PCM") 
         sys.exit(-1)   
-        
+    
     maxsegments = math.floor(duration*samplerate/observationperiod)
     resultarr = np.zeros((maxsegments), dtype=np.uint) 
     for idx in range(0, maxsegments):
         # To speed things up, only check in the expected neighborhood of the segment (e.g., +/- 500mS).
         neighborstart = (idx*observationperiod)-(neighborhood//2)
+        neighborend   = (idx*observationperiod)+(neighborhood//2)
         if neighborstart < 0:
             neighborstart = 0
-        neighborend   = (idx*observationperiod)+(neighborhood//2)
+            neighborend = neighborhood
         if neighborend > len(filedata):
+            neighborstart = len(filedata) - neighborhood
             neighborend = len(filedata)
-
-        subjectdata = filedata[neighborstart:neighborend].copy()
-        thissegment = segmentdataPN[(idx*observationperiod):((idx+1)*observationperiod)].copy()
-        resultarr[idx] = gettimefromsegment(subjectdata, thissegment, cyclicpad, NONE_TYPE) + neighborstart
+        
+        subjectdata = filedata[neighborstart:neighborend].view()
+        thissegment = segmentdataPN[(idx*observationperiod):((idx+1)*observationperiod)].view()
+        resultraw = gettimefromsegment(subjectdata, thissegment) + neighborstart
+        
+        # Clean up error due to differences in playout and record sample clock rates; we assume the error is small w.r.t. the correct value. 
+        resultclean = int(round(resultraw/observationperiod,0)*observationperiod)
+        resultarr[idx] = resultclean 
         
     return resultarr
 
