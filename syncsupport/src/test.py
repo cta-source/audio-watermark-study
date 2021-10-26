e9c8173b38d7aa66f240ffb34ba42ac093d74860
cta-source <source@cta.tech>
2021-10-26 08:53:49 -0400

    Update test.py
    Removed signal conditioning (not helpful for short segments); improved the segment-out-of-order check.

--- syncsupport/src/test.py
+++ syncsupport/src/test.py
@@ -12,32 +12,38 @@ import time
 from syncsupport import findsegmentorder
 
 # Default parameters
-# RATE = 48000              # Audio sample rate  (not used here except in the comments; this rate is a hard-coded value in syncsupport.
-observationperiod = 960    # This value works out to 80mS.  Target should be 960 (20mS).  However, the signal-to-noise in the test file is too great for 20mS to work.
-cyclicpad = 0               # (units are 1/RATE seconds) For signal conditioning, usually some number of audio samples at 48kHz sample rate. "0" means "not used".  For short (20-80mS) observation period, not helpful so keep at 0.
-neighborhood = 48000        # (units are 1/RATE seconds) Checking each segment of a PN sequence against 60 seconds of audio takes too long.  Only check in the "neighborhood" of where the segment is expected to be.
+samplerate = 48000          # Audio sample rate  (not used here except in the comments; this rate is a hard-coded value in the content and PN files).
+observationperiod = 960     # This value works out to 20mS at a 48 kHz audio sample rate.
+neighborhood = 25*observationperiod  # Units are 1/RATE seconds. Checking each segment of a PN sequence against 60 seconds of audio takes too long.  Only check in the "neighborhood" of where the segment is expected to be.
 
 # Test files
-subjectfile = "./src/AdagioPN01-12dB2snoise1.wav"  # Demonstration file with 2 seconds of random noise, 60 seconds of an adagio with PN01 noise, and 2 seconds of random noise.
-segmentfile = "./src/PN01.wav"
+segmentfile = "./src/PN01.wav"  # Reference (a priori) watermark filename.
+# Choose one of:
+subjectfile = "./src/PN01.wav"                                          # Simplest test, verify the software can sync PN01 to itself.
+#subjectfile = "./PN01 FS 0 s to 1s stepped -1dB per sec w Adagio 5s-6s repeat FS.wav"  # Demonstration file generated on Audacity, a 1s loop of Adagio repeats for 60s, mixed with PN01 at full scale at t=0, -1dB starting t=1s, -2dB at 2s etc.
+#subjectfile = "./Recorded - basic PN01 w mic close to speakers.wav"     # Demonstration file, playout of PN01 over speakers on one PC, and recorded via mic on a second PC.
+#subjectfile = "./Recorded - basic PN01 wired via USB input device.wav"  # Demonstration file, playout of PN01 on one PC, and recorded via direct wired connection (line-out to line-in) on a second PC.
 
 print("\n    enter findsegmentorder at "+str(time.asctime(time.gmtime())))
-segmenttimingarray = findsegmentorder(subjectfile, segmentfile, observationperiod, cyclicpad, neighborhood)
+segmenttimingarray = findsegmentorder(subjectfile, segmentfile, observationperiod, neighborhood)
 print("\n    leave findsegmentorder at "+str(time.asctime(time.gmtime())))
 
 # Check the result, satisfying "all segments in order" requirement in DPCTF spec
 segmentcount = len(segmenttimingarray)
 errorcount = 0
 for idx in range(1, segmentcount):
-    if segmenttimingarray[idx] <= segmenttimingarray[idx-1]:
+    if ((idx)*observationperiod) != segmenttimingarray[idx]:
         print("Error, segments out of order, expected "+str((idx-1)*observationperiod)+", found "+str(segmenttimingarray[idx]))
         errorcount += 1
-        #sys.exit(-1)
+        if errorcount > 500:
+            print("Error: found greater than "+str(errorcount)+" segments out of order; terminating program")
+            print("\n    Finish and terminate at "+str(time.asctime(time.gmtime())))
+            sys.exit(-1)
 
 if errorcount == 0:
     print("Success -- all segments in order")
 else:
     print("Error: found "+str(errorcount)+" segments out of order")
+print("\n    Finish and terminate at "+str(time.asctime(time.gmtime())))
     
 sys.exit(1)   
-
